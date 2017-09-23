2346d4e5bf610f53207b34d3b44cc12135bfa16a
daguan <daguan@fudimen.com>
2017-09-23 18:57:26 +0800

    排序二叉树查找数组所有最大值

---
+++ RetuenLargeNumbersInArray.js
@@ -0,0 +1,96 @@
+/*
+* @param 二叉树排序查找最大值
+* @param Author@daguan.fudimen.com 
+ */
+
+var Binary = function()
+{
+  var Node = function ( key)
+  {
+    this.left = null;
+    this.right = null;
+    this.key = key;
+  };
+  
+  var root = null;
+  var insertNode = function(node, newNode)
+  {
+     if(node.key > newNode.key)
+       {
+          if(node.left === null)
+            {
+              node.left = newNode;
+            }
+         else
+           {
+              insertNode(node.left, newNode);
+            }
+       }
+    else 
+      {
+        if(node.right === null)
+            {
+              node.right = newNode;
+            }
+         else
+           {
+              insertNode(node.right, newNode);
+            }
+      }
+  };
+  
+  this.insert = function (key)
+  {
+     var newNode = new Node(key);
+     if(root === null)
+        {
+            root = newNode;
+        }
+     else
+      {
+        insertNode(root, newNode);
+      }
+  };
+  
+  var findMax = function (node)
+  {
+      if(node === null)
+        {
+          return 0;
+        }
+      
+    if(node.right !== null)
+      {
+        return findMax(node.right);
+      }
+    return node.key;
+  };
+  
+  this.max = function ()
+  {
+     return findMax(root);
+  };
+};
+
+
+function largestOfFour(arr) {
+  // 请把你的代码写在这里
+  var newArray = [];
+  
+  
+  for(var i = 0; i<arr.length; i++)
+    {
+        var binary = new Binary();
+        arr[i].map(
+          function (val)
+          {
+            console.log(val);
+            binary.insert(val);
+          }
+        );
+        newArray.push(binary.max());
+    }
+  return newArray;
+}
+
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
